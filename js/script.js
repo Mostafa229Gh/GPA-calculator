37b9d0981f111a70ac06b51beaa5867b762a6acb
Mostafa <mostafa229gh@gmail.com>
2024-04-21 20:27:09 +0330

    'dark mode' phases finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
 const container = document.querySelector(':root');
+const darkMode = document.getElementById('dark-mode-toggle');
+let isMoved = false;
 
-darkModeToggle.addEventListener('click', () => {
+darkMode.addEventListener('click', () => {
     container.classList.toggle('dark-mode');
+    
+    if (!isMoved) {
+        darkMode.style.left = '2.7vw';
+        darkMode.style.backgroundImage = "url('image/icons-sun.png')";
+
+    } else {
+        darkMode.style.left = '7.3vw'; 
+        darkMode.style.backgroundImage = "url('image/icons-moon.png')";
+    }
+    isMoved = !isMoved; 
 });
+// moveMoon.addEventListener('click', (event) => {
+//     const offsetX = event.clientX - event.target.offsetLeft;
+//     const offsetY = event.clientY - event.target.offsetTop;
+    
+//     moveMoon.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+// });
