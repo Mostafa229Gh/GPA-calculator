bd858d929f9ae489a00b18e41635626d6537b5f0
Mostafa <mostafa229gh@gmail.com>
2024-04-23 15:53:12 +0330

    plusButton actived, change footer

--- js/script.js
+++ js/script.js
@@ -1,23 +1,39 @@
-const container = document.querySelector(':root');
-const darkMode = document.getElementById('dark-mode-toggle');
+//---Dark Mode Toggle
+const container = document.querySelector(":root");
+const darkMode = document.getElementById("dark-mode-toggle");
 let isMoved = false;
 
-darkMode.addEventListener('click', () => {
-    container.classList.toggle('dark-mode');
-    
-    if (!isMoved) {
-        darkMode.style.left = '2.7vw';
-        darkMode.style.backgroundImage = "url('image/icons-sun.png')";
+darkMode.addEventListener("click", () => {
+  container.classList.toggle("dark-mode");
 
-    } else {
-        darkMode.style.left = '7.3vw'; 
-        darkMode.style.backgroundImage = "url('image/icons-moon.png')";
-    }
-    isMoved = !isMoved; 
+  if (!isMoved) {
+    darkMode.style.left = "2.7vw";
+    darkMode.style.backgroundImage = "url('image/icons-sun.png')";
+  } else {
+    darkMode.style.left = "7.3vw";
+    darkMode.style.backgroundImage = "url('image/icons-moon.png')";
+  }
+  isMoved = !isMoved;
 });
-// moveMoon.addEventListener('click', (event) => {
-//     const offsetX = event.clientX - event.target.offsetLeft;
-//     const offsetY = event.clientY - event.target.offsetTop;
+
+//---Clone input tables
+let i = 0;
+const tables = document.getElementById("duplicater");
+
+function duplicate() {
+  const clone = tables.cloneNode(true);
+  clone.id = "duplicater" + ++i;
+  tables.parentNode.insertBefore(clone, puls);
+}
+//--Move Button
+const puls = document.getElementById("plus");
+let isClick = false;
+
+puls.addEventListener("click", () => {
+  if (!isClick) {
+    tables.style = "display: block";
+    puls.style = "transform: translate(-31vw, -26vh) scale(0.2)";
     
-//     moveMoon.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-// });
+    isClick = true;
+  }
+});
